9ff46d62f018e2e7b1473848cb4a95fa0fc055ef
DiegoPereiro <diegocfpereiro@gmail.com>
2018-11-05 23:02:59 +0100

    Tarea finalizada

--- Ejercicio 2/Lenguaje/src/Lenguaje.java
+++ Ejercicio 2/Lenguaje/src/Lenguaje.java
@@ -7,9 +7,16 @@ import java.util.Random;
 
 public class Lenguaje {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
+		
+//		Integer cantidadGrupos =40;
+//		String rutaFichero =  "F:\\Documentos\\Ciclo DAM\\Segundo curso\\Programacion de Servicios e Procesos\\UD01\\Tarea 1\\Ejercicio 2\\prueva.txt";
+//		
 		Integer cantidadGrupos =Integer.parseInt(args[0]);
 		String rutaFichero =  args[1];
+				
+		
+		
 
 		Random Letras = new Random();
 		FileLock bloqueo;
@@ -24,11 +31,12 @@ public class Lenguaje {
 				
 				for (int j = 0; j <Letras.nextInt(20)+5; j++) { //repetimos entre 5 y 25 para generar el grupo de letras				
 					grupo=grupo+(char)(Letras.nextInt(26)+97);//generamos numeros aleatorios entre 97 y 122 para cuando las pasemos a char sean las letras minusculas
-				}		
+				}	
+				randomAccessFile.seek(randomAccessFile.length());//Nos situa al final del fichero	
 				randomAccessFile.writeBytes(grupo+"\n");	//escribimos los grupos en el fichero						
 				grupo="";				
 			}			
-			randomAccessFile.seek(0); //Nos situa al principio del fichero
+//			randomAccessFile.seek(0); //Nos situa al principio del fichero
 			bloqueo.release();//Libera el bloqueo
 
 
