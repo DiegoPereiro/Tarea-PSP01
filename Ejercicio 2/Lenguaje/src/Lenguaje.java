c70553563056fe0fcd894056139a462d1eb593dc
DiegoPereiro <diegocfpereiro@gmail.com>
2018-11-05 21:33:35 +0100

    ejercicio 2 - lenguaje

---
+++ Ejercicio 2/Lenguaje/src/Lenguaje.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileLock;
+import java.util.Random;
+
+public class Lenguaje {
+
+	public static void main(String[] args) {
+		Integer cantidadGrupos =Integer.parseInt(args[0]);
+		String rutaFichero =  args[1];
+
+		Random Letras = new Random();
+		FileLock bloqueo;
+
+		try {
+			File fichero = new File(rutaFichero);
+			RandomAccessFile randomAccessFile = new RandomAccessFile(fichero, "rwd"); // crear el acceso al fichero
+			bloqueo = randomAccessFile.getChannel().lock();//bloquamos el acceso al fichero		
+
+			String grupo = "";
+			for (int i = 0; i <cantidadGrupos; i++) {//este bucle se repetira tantas veces como venga especificado por el usuario
+				
+				for (int j = 0; j <Letras.nextInt(20)+5; j++) { //repetimos entre 5 y 25 para generar el grupo de letras				
+					grupo=grupo+(char)(Letras.nextInt(26)+97);//generamos numeros aleatorios entre 97 y 122 para cuando las pasemos a char sean las letras minusculas
+				}		
+				randomAccessFile.writeBytes(grupo+"\n");	//escribimos los grupos en el fichero						
+				grupo="";				
+			}			
+			randomAccessFile.seek(0); //Nos situa al principio del fichero
+			bloqueo.release();//Libera el bloqueo
+
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+
+	}
+
+}
